666ee390e0902a7b8ce4e8eb638a14c89fb2bf87
BEATRIZ <99761092+beaosma@users.noreply.github.com>
2022-05-31 07:16:32 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,63 @@
+input.onGesture(Gesture.Shake, function () {
+    NÚMERO = randint(0, 6)
+    if (NÚMERO == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (NÚMERO == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (NÚMERO == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    }
+    if (NÚMERO == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (NÚMERO == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (NÚMERO == 6) {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . # . #
+            `)
+    }
+})
+let NÚMERO = 0
+basic.clearScreen()
+NÚMERO = 0
+basic.forever(function () {
+	
+})
